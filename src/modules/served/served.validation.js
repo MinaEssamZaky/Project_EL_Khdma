4b16b4600776707a15c7a07b6729569ee0c77def
MinaEssamZaky <minaessam856@gmail.com>
2025-06-12 15:58:03 +0300

    served Update

--- src/modules/served/served.validation.js
+++ src/modules/served/served.validation.js
@@ -10,9 +10,9 @@ export const addServedValidation = Joi.object({
     address: Joi.string().min(5).max(50).required(),
     address2: Joi.string().min(5).max(50).allow('', null), // Allow empty or null
     mobileNumber1: Joi.string().length(11).required().pattern(/^[0-9]+$/),
-    mobileNumber2: Joi.string().length(11).empty().allow(null).pattern(/^[0-9]*$/).optional(), // Allow empty or null
+    mobileNumber2: Joi.string().length(11).empty('').allow(null).pattern(/^[0-9]*$/).optional(), // Allow empty or null
     IsExpatriate: Joi.boolean(),
-    Landline: Joi.string().length(9).empty().allow(null).pattern(/^[0-9]*$/).optional(),
+    Landline: Joi.string().length(9).empty('').allow(null).pattern(/^[0-9]*$/).optional(),
     Church: Joi.string().min(2).max(50).required(),
     priestName: Joi.string().min(2).max(50),
     collegeOrInstitute: Joi.string().min(2).max(50),
