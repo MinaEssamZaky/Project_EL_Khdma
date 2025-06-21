4d8a8fc45006647cd788f8af5e69890149603515
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-21 10:33:51 +0300

    Update served.validation.js

--- src/modules/served/served.validation.js
+++ src/modules/served/served.validation.js
@@ -22,5 +22,5 @@ export const addServedValidation = Joi.object({
     maritalStatus: Joi.string().valid("Single", "Married", "Engaged").required(),
     cohort: Joi.string().min(2).max(50),
     Profession: Joi.string().min(2).max(50).allow('', null),
-    dayOff: Joi.string().valid("Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday")
+    dayOff: Joi.string().valid("Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday").allow(null).empty('').optional()
 });
