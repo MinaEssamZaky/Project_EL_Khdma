93d84bb4f91620f036e4ca70fb2b7fc3066d3a86
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-22 09:38:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ import { servedRouter } from './src/modules/served/served.routes.js'
 dotenv.config()
 const app = express()
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+  origin: '*', // أو origin بتاع الفرونت إند
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 //userRouter
 app.use("/api/v1/user",userRouter)
