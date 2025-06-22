38402d91203d98103faabad4939cce623694e8e2
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-22 11:04:39 +0300

    Update served.routes.js

--- src/modules/served/served.routes.js
+++ src/modules/served/served.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addServed,gitAllServeds } from "./served.controller.js";
+import { addServed , getAllServeds } from "./served.controller.js";
 import { auth, authorizeRoles } from "../../middleware/auth.js";
 import { validation } from "../../middleware/Validation.js";
 import { addServedValidation  } from "./served.validation.js";
@@ -8,4 +8,4 @@ import { addServedValidation  } from "./served.validation.js";
 export const servedRouter = express.Router();
 
 servedRouter.post("/addServed", validation(addServedValidation),auth(),authorizeRoles("Admin"), addServed)
-servedRouter.git("/gitAllServeds",auth(),authorizeRoles("Admin,SuperAdmin"), gitAllServeds)
+servedRouter.git("/getAllServeds ",auth(),authorizeRoles("Admin,SuperAdmin"), getAllServeds )
