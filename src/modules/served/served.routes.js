0ab88a2b1b19e270cec234241b3b10cfbe5ee2df
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-22 11:51:47 +0300

    Update served.routes.js

--- src/modules/served/served.routes.js
+++ src/modules/served/served.routes.js
@@ -1,12 +1,13 @@
 import express from "express";
-import { addServed , getAllServeds } from "./served.controller.js";
+import { addServed , getAllServeds ,updateServed} from "./served.controller.js";
 import { auth, authorizeRoles } from "../../middleware/auth.js";
 import { validation } from "../../middleware/Validation.js";
-import { addServedValidation  } from "./served.validation.js";
+import { addServedValidation ,updateServedValidation  } from "./served.validation.js";
 
 
 export const servedRouter = express.Router();
 
 servedRouter.post("/addServed", validation(addServedValidation),auth(),authorizeRoles("Admin"), addServed)
 servedRouter.get("/getAllServeds", auth(), authorizeRoles("Admin", "SuperAdmin"), getAllServeds);
+servedRouter.put('/updateServed/:id',  validation(updateServedValidation),auth(), authorizeRoles("Admin", "SuperAdmin"), updateServed);
 
