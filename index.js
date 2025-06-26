c93b5b532a4e4a6d4e80f6fba1c94955c629ad2f
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-26 12:25:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-  origin: ['http://localhost:3000','https://ugm-family.vercel.app/'], 
+  origin: ['http://localhost:3000','https://ugm-family.vercel.app'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
