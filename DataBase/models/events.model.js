0b6f353fefe803ba45d2af3d61cf9e82551b07e7
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-26 11:56:19 +0300

    Update events.model.js

--- DataBase/models/events.model.js
+++ DataBase/models/events.model.js
@@ -1,1 +1,18 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema({
+  eventName:        { type: String, required: true },
+  category:         { type: String, required: true , enum:["event","trip"]},
+  date:             { type: Date,   required: true },
+  address:          { type: String, required: true },
+  shortDescription: { type: String, required: true },
+  fullDescription:  { type: String, required: true },
+  responsiblePerson:{ type: String, required: true },
+  phone:            { type: String, required: true },
+  price:            { type: Number, required: true },
+  images:           { type: [String], required: true},
+}, { timestamps: true });
+
+const eventModel = mongoose.model("event", eventSchema);
+export default eventModel;
 
