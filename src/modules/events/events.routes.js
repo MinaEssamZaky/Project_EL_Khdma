fc615d985a0a0f5bf255cf79d54e986843db2f4e
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-29 10:09:33 +0300

    Update events.routes.js

--- src/modules/events/events.routes.js
+++ src/modules/events/events.routes.js
@@ -1,1 +1,10 @@
+import express from "express";
+import { upload } from "../../../utils/fileUpload.js";
+import { createEvent } from "./event.controller.js";
+
+const eventRoute = express.Router();
+
+eventRoute.post("/addEvent", upload.array("images", 5), createEvent);
+
+export default eventRoute;
 
