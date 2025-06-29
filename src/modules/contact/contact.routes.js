47eb2cc4d0251dbff363de43d5690decdb2ecb07
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-29 12:45:14 +0300

    Update contact.routes.js

--- src/modules/contact/contact.routes.js
+++ src/modules/contact/contact.routes.js
@@ -1,1 +1,13 @@
+import express from "express";
+import { addMessage , getAllMessage ,deleteMessage} from "./contact.controller.js";
+import { auth, authorizeRoles } from "../../middleware/auth.js";
+import { validation } from "../../middleware/Validation.js";
+import { addMessageValidation } from "./contact.validation.js";
+
+
+export const contactRouter = express.Router();
+
+contactRouter.post("/addMessage", validation(addMessageValidation),auth(),authorizeRoles("SuperAdmin"),addMessage)
+contactRouter.get("/getAllMessage", auth(), authorizeRoles("SuperAdmin"), getAllMessage);
+contactRouter.delete("/deleteMessage/:id", auth(), authorizeRoles("SuperAdmin"), deleteMessage);
 
