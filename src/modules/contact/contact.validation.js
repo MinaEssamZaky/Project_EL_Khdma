a9c7b42cadaaa9a6b512ba7eeed176b18feb8204
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-29 12:52:20 +0300

    Update contact.validation.js

--- src/modules/contact/contact.validation.js
+++ src/modules/contact/contact.validation.js
@@ -1,1 +1,7 @@
-contact
+import Joi from "joi"
+
+export const addMessageValidation= joi.object ({
+  userName= joi.string().min(5).max(20).required(),
+  message= joi.string().max(500).required(),
+   phone= joi.string().min(11).max(11).required()
+})
