80406cc1c449e7369660d43c354f12c27c92e3ee
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-06-29 08:43:55 +0300

    Update DbConnection.js

--- DataBase/DbConnection.js
+++ DataBase/DbConnection.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 
-export const DataBaseConnection =  () => {
-    mongoose.connect(process.env.DataBase_URL) .then(() => {
+export const DataBaseConnection = async  () => {
+  await mongoose.connect(process.env.DataBase_URL) .then(() => {
         console.log("DataBase Connected");
     }).catch((err) => {
         console.log("DataBase Connection Error", err);})
