7fc89161075940baeb776498f0d2170ea8333267
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-01 11:24:00 +0300

    Update events.controller.js

--- src/modules/events/events.controller.js
+++ src/modules/events/events.controller.js
@@ -1,8 +1,15 @@
 import eventModel from "../../../DataBase/models/events.model.js";
+import userModel from "../../../DataBase/models/user.model.js";
 import { AppError } from "../../utils/AppError.js";
 import { handleError } from "../../middleware/HandleError.js";
 
 export const createEvent = handleError(async (req, res, next) => {
+   if (req.user.role !== 'SuperAdmin' ) {return next(new AppError("Access Denied", 403));}
+  
+            const user = await userModel.findById(req.user._id)
+                if(!user){return next(new AppError("User not found",404));}
+
+  
   const { eventName, category, date, address, shortDescription, fullDescription, responsiblePerson, phone, price } = req.body;
 
   if (!req.files || req.files.length === 0) {
