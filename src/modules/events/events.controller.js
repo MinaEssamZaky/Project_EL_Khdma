0f34e4b492e5fe556ab975140085f59c7a2e842f
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-01 11:09:11 +0300

    Update events.controller.js

--- src/modules/events/events.controller.js
+++ src/modules/events/events.controller.js
@@ -1,1 +1,40 @@
+import express from "express";
+import { AppError } from "../../utils/AppError.js";
+import eventModel from "../../../DataBase/models/events.model.js";
+import { handleError } from "../../middleware/HandleError.js";
+import userModel from "../../../DataBase/models/user.model.js";
 
+
+export const addEvent = handleError(async (req, res, next) => {
+  
+    const {userName,phone,message} = req.body;
+
+    const newMessage = await eventModel.create({userName,phone,message});
+
+    res.status(201).json({ message: "Successfully", newMessage });
+});
+
+
+export const getAllMessage = handleError(async (req, res, next) => {
+
+          if (req.user.role !== 'SuperAdmin' ) {return next(new AppError("Access Denied", 403));}
+  
+            const user = await userModel.findById(req.user._id)
+                if(!user){return next(new AppError("User not found",404));}
+        
+        const getAllMessage = await contactModel.find();
+        res.status(201).json({ message: "Successfully", getAllMessage });
+})
+
+export const deleteMessage = handleError(async (req, res, next) => {
+  if ( req.user.role !== 'SuperAdmin' ) {return next(new AppError("Access Denied", 403));}
+  
+  const messageId = req.params.id;
+  const deleted = await contactModel.findByIdAndDelete(messageId);
+
+  if (!deleted) {
+    return next(new AppError("Contact not found", 404));
+  }
+
+  return res.status(200).json({ message: "Contact Deleted Successfully" });
+});
