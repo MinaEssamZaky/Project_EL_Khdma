228118f1155594cae1733d7d91fb66a540b8ee6b
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-01 16:31:23 +0300

    Update events.routes.js

--- src/modules/events/events.routes.js
+++ src/modules/events/events.routes.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { auth, authorizeRoles } from "../../middleware/auth.js";
+import { validation } from "../../middleware/Validation.js";
 import { createEvent } from "./events.controller.js";
 import {eventValidation} from "./events.validation.js"
 
