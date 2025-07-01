c0ec277d9134e0548f5386bef9a30c632ba33821
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-01 14:34:32 +0300

    Update events.validation.js

--- src/modules/events/events.validation.js
+++ src/modules/events/events.validation.js
@@ -1,13 +1,14 @@
-// import Joi from "joi";
+import Joi from "joi";
 
-// export const eventValidation = Joi.object({
-//   eventName: Joi.string().min(3).max(50).required(),
-//   category: Joi.string().valid("event", "trip").required(),
-//   date: Joi.date().required(),
-//   address: Joi.string().min(5).max(100).required(),
-//   shortDescription: Joi.string().min(10).max(200).required(),
-//   fullDescription: Joi.string().min(20).max(1000).required(),
-//   responsiblePerson: Joi.string().min(3).max(50).required(),
-//   phone: Joi.string().length(11).pattern(/^[0-9]+$/).required(),
-//   price: Joi.number().min(0).required(),
-// });
+export const eventValidation = Joi.object({
+  eventName: Joi.string().min(3).max(50).required(),
+  category: Joi.string().valid("event", "trip").required(),
+  date: Joi.date().required(),
+  address: Joi.string().min(5).max(100).required(),
+  shortDescription: Joi.string().min(10).max(200).required(),
+  fullDescription: Joi.string().min(20).max(1000).required(),
+  responsiblePerson: Joi.string().min(3).max(50).required(),
+  phone: Joi.string().length(11).pattern(/^[0-9]+$/).required(),
+  price: Joi.number().min(0).required(),
+  image:Joi.array().items(Joi.string()).min(2).max(5) // بين 2 إلى 5 عناصر
+});
