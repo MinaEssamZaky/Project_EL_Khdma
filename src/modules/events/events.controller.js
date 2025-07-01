85dd29d50ab67da35f22e5675e26a75c87746fa1
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-01 14:27:03 +0300

    Update events.controller.js

--- src/modules/events/events.controller.js
+++ src/modules/events/events.controller.js
@@ -1,35 +1,29 @@
-// import eventModel from "../../../DataBase/models/events.model.js";
-// import userModel from "../../../DataBase/models/user.model.js";
-// import { AppError } from "../../utils/AppError.js";
-// import { handleError } from "../../middleware/HandleError.js";
+import eventModel from "../../../DataBase/models/events.model.js";
+import userModel from "../../../DataBase/models/user.model.js";
+import { AppError } from "../../utils/AppError.js";
+import { handleError } from "../../middleware/HandleError.js";
 
-// export const createEvent = handleError(async (req, res, next) => {
-//    if (req.user.role !== 'SuperAdmin' ) {return next(new AppError("Access Denied", 403));}
+export const createEvent = handleError(async (req, res, next) => {
+   if (req.user.role !== 'SuperAdmin' ) {return next(new AppError("Access Denied", 403));}
   
-//             const user = await userModel.findById(req.user._id)
-//                 if(!user){return next(new AppError("User not found",404));}
+            const user = await userModel.findById(req.user._id)
+                if(!user){return next(new AppError("User not found",404));}
 
   
-//   const { eventName, category, date, address, shortDescription, fullDescription, responsiblePerson, phone, price } = req.body;
+  const { eventName, category, date, address, shortDescription, fullDescription, responsiblePerson, phone, price } = req.body;
 
-//   if (!req.files || req.files.length === 0) {
-//     return next(new AppError("Images are required", 400));
-//   }
+  const newEvent = await eventModel.create({
+    eventName,
+    category,
+    date,
+    address,
+    shortDescription,
+    fullDescription,
+    responsiblePerson,
+    phone,
+    price,
+    images,
+  });
 
-//   const images = req.files.map(file => file.path); // استخراج روابط الصور من Cloudinary
-
-//   const newEvent = await eventModel.create({
-//     eventName,
-//     category,
-//     date,
-//     address,
-//     shortDescription,
-//     fullDescription,
-//     responsiblePerson,
-//     phone,
-//     price,
-//     images,
-//   });
-
-//   res.status(201).json({ message: "Event created successfully", event: newEvent });
-// });
+  res.status(201).json({ message: "Event created successfully", event: newEvent });
+});
