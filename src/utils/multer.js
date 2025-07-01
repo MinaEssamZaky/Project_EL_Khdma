706676b854f5a85bb69d02adae2e936182cc06d1
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-01 11:04:59 +0300

    Create multer.js

---
+++ src/utils/multer.js
@@ -0,0 +1,14 @@
+const { storage } = require('./cloudinary');
+const multer = require('multer');
+
+export const upload = multer({ 
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+      cb(null, true);
+    } else {
+      cb(new Error('Only image files are allowed!'), false);
+    }
+  }
+});
+  
