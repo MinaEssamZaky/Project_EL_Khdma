cfe898865eb289fe6cbec37c236bab8490ca5254
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-02 10:12:30 +0300

    Update events.routes.js

--- src/modules/events/events.routes.js
+++ src/modules/events/events.routes.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { auth, authorizeRoles } from "../../middleware/auth.js";
 import { validation } from "../../middleware/Validation.js";
-import { createEvent } from "./events.controller.js";
+import { createEvent,getAllEvents,getEventById,deleteEvent} from "./events.controller.js";
 import {eventValidation} from "./events.validation.js"
 
 const eventsRouter = express.Router();
 
 eventsRouter.post("/addEvent",auth(),authorizeRoles("Admin", "SuperAdmin"),createEvent);
+eventsRouter.get("/getAllEvents",auth(),getAllEvents);
+eventsRouter.get("/getEventById/:id",auth(),getEventById);
+eventsRouter.post("/deleteEvent/:id",auth(),authorizeRoles("Admin", "SuperAdmin"),deleteEvent);
 
 export default eventsRouter;
 
