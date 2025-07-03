35d13c8ef98adcaaa6fe2370e5cf0d8049015a6d
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-03 12:15:20 +0300

    Update memory.controller.js

--- src/modules/memory/memory.controller.js
+++ src/modules/memory/memory.controller.js
@@ -4,6 +4,9 @@ import { handleError } from "../../middleware/HandleError.js";
 
 
 export const uploadImage = handleError(async (req, res, next) => {
+  if (!req.files || req.files.length === 0) {
+    return next(new AppError("No images were uploaded", 400));
+  }
    const images = req.files.map(file => file.path);
   
   if(!images){next(new AppError("Upload failed",500))}
