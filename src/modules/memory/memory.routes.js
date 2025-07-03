7d7c02922e43b20479a1d8281fb21d45f9c8b8ba
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-03 08:50:30 +0300

    Update memory.routes.js

--- src/modules/memory/memory.routes.js
+++ src/modules/memory/memory.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import multer from 'multer';
 import { uploadImage } from './memory.controller.js'; // تأكد من مسار الملف الصحيح
+import { storage } from '../utils/cloudinary.config.js';
+
 const uploadRouter = express.Router();
 // إعداد multer لرفع الملفات باستخدام Cloudinary
-const upload = multer({ storage: multer.memoryStorage() }); // استخدام الذاكرة لتخزين الملفات مؤقتًا
+const upload = multer({ storage}); // استخدام الذاكرة لتخزين الملفات مؤقتًا
 
 uploadRouter.post('/uploadPhotos', upload.array('image'), uploadImage);
 
