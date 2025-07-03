2fb81c216d4a31a2fd1e3f76885c4aeaf8e93cc6
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-03 11:41:08 +0300

    Update memory.controller.js

--- src/modules/memory/memory.controller.js
+++ src/modules/memory/memory.controller.js
@@ -5,11 +5,8 @@ import { handleError } from "../../middleware/HandleError.js";
 
 export const uploadImage = handleError(async (req, res, next) => {
    const images = req.files.map(file => file.path);
-  if(!images){
-    return AppError({"Upload failed",500})
-  }
-      res.status(200).json({
-      message: 'Images uploaded successfully',
-      imageUrls: images
-    });
+  
+  if(!images){next(new AppError("Upload failed",500))}
+  
+      res.status(200).json({message: 'Images uploaded successfully',imageUrls: images});
 });
