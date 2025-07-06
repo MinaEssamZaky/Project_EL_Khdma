5e7c165ddfaa26bb68bffa063834d52e3b872fb4
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-06 14:25:35 +0300

    Update emailTemplate.js

--- src/emails/emailTemplate.js
+++ src/emails/emailTemplate.js
@@ -31,17 +31,11 @@ export function emailTemplate(token){
 										<tr>
 											<td style="background-color:#00d2f4;font-size:1px;line-height:3px" class="topBorder" height="3">&nbsp;</td>
 										</tr>
-										<tr>
-											<td style="padding-top: 60px; padding-bottom: 20px;" align="center" valign="middle" class="emailLogo">
-												<a href="#" style="text-decoration:none" target="_blank">
-													<img alt="" border="0" src="http://email.aumfusion.com/vespro/img/hero-img/blue/logo.png" style="width:100%;max-width:150px;height:auto;display:block" width="150">
-												</a>
-											</td>
-										</tr>
+										
 										<tr>
 											<td style="padding-bottom: 20px;" align="center" valign="top" class="imgHero">
 												<a href="#" style="text-decoration:none" target="_blank">
-													<img alt="" border="0" src="http://email.aumfusion.com/vespro/img/hero-img/blue/heroGradient/user-account.png" style="width:100%;max-width:600px;height:auto;display:block;color: #f9f9f9;" width="600">
+													<img alt="" border="0" src="https://i.postimg.cc/xjpwg33Y/479983623-122120879072672558-4344917141121777292-n.jpg" style="width:100%;max-width:600px;height:auto;display:block;color: #f9f9f9;" width="600">
 												</a>
 											</td>
 										</tr>
@@ -76,7 +70,7 @@ export function emailTemplate(token){
 																<table border="0" cellpadding="0" cellspacing="0" align="center">
 																	<tbody>
 																		<tr>
-																			<td style="background-color: rgb(0, 210, 244); padding: 12px 35px; border-radius: 50px;" align="center" class="ctaButton"> <a href="https://ugmproject.vercel.app/verifyEmail?token=${token}" style="color:#fff;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:13px;font-weight:600;font-style:normal;letter-spacing:1px;line-height:20px;text-transform:uppercase;text-decoration:none;display:block" target="_blank" class="text">Confirm Email</a>
+																			<td style="background-color: rgb(0, 210, 244); padding: 12px 35px; border-radius: 50px;" align="center" class="ctaButton"> <a href="https://ugm-family.vercel.app/verifyEmail?token=${token}" style="color:#fff;font-family:Poppins,Helvetica,Arial,sans-serif;font-size:13px;font-weight:600;font-style:normal;letter-spacing:1px;line-height:20px;text-transform:uppercase;text-decoration:none;display:block" target="_blank" class="text">Confirm Email</a>
 																			</td>
 																		</tr>
 																	</tbody>
@@ -90,15 +84,7 @@ export function emailTemplate(token){
 										<tr>
 											<td style="font-size:1px;line-height:1px" height="20">&nbsp;</td>
 										</tr>
-										<tr>
-											<td align="center" valign="middle" style="padding-bottom: 40px;" class="emailRegards">
-												<!-- Image and Link // -->
-												<a href="#" target="_blank" style="text-decoration:none;">
-													<img mc:edit="signature" src="http://email.aumfusion.com/vespro/img//other/signature.png" alt="" width="150" border="0" style="width:100%;
-    max-width:150px; height:auto; display:block;">
-												</a>
-											</td>
-										</tr>
+										
 									</tbody>
 								</table>
 								<table border="0" cellpadding="0" cellspacing="0" width="100%" class="space">
@@ -137,11 +123,7 @@ export function emailTemplate(token){
 												</a>
 											</td>
 										</tr>
-										<tr>
-											<td style="padding: 10px 10px 5px;" align="center" valign="top" class="brandInfo">
-												<p class="text" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0">©&nbsp;Vespro Inc. | 800 Broadway, Suite 1500 | New York, NY 000123, USA.</p>
-											</td>
-										</tr>
+										
 										<tr>
 											<td style="padding: 0px 10px 20px;" align="center" valign="top" class="footerLinks">
 												<p class="text" style="color:#bbb;font-family:'Open Sans',Helvetica,Arial,sans-serif;font-size:12px;font-weight:400;font-style:normal;letter-spacing:normal;line-height:20px;text-transform:none;text-align:center;padding:0;margin:0"> <a href="#" style="color:#bbb;text-decoration:underline" target="_blank">View Web Version </a>&nbsp;|&nbsp; <a href="#" style="color:#bbb;text-decoration:underline" target="_blank">Email Preferences </a>&nbsp;|&nbsp; <a href="#" style="color:#bbb;text-decoration:underline" target="_blank">Privacy Policy</a>
