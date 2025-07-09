111b4ed870caeb057bc95b69590492b4af1ba214
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-09 10:38:00 +0300

    Update booking.routes.js

--- src/modules/booking/booking.routes.js
+++ src/modules/booking/booking.routes.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import {createBookingByWallet} from "../booking.controller.js"
-import auth from "../../utl"
+import multer from 'multer';
+import { auth, authorizeRoles } from "../../middleware/auth.js";
+import { storage } from "../../utils/cloudinary.config.js"; // تأكد من مسار الملف الصحيح
+
 const bookingRouter = express.Router();
 
 bookingRouter.Post("/BookingByWallet",auth(),createBookingByWallet)
