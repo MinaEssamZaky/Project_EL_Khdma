6961e224dbedc927d0b9bf34914855563acb50f9
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-09 17:16:57 +0300

    Update events.routes.js

--- src/modules/events/events.routes.js
+++ src/modules/events/events.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 import multer from 'multer';
 import { auth, authorizeRoles } from "../../middleware/auth.js";
 import { validation } from "../../middleware/Validation.js";
-import { createEvent,getAllEvents,getEventById,deleteEvent,getEventReservedsById} from "./events.controller.js";
+import { createEvent,getAllEvents,getEventById,deleteEvent,getEventReservedsById,getAllEventsReserveds} from "./events.controller.js";
 import {eventValidation} from "./events.validation.js"
 import { storage } from "../../utils/cloudinary.config.js"; // تأكد من مسار الملف الصحيح
 
@@ -12,13 +12,14 @@ const upload = multer({ storage}); // استخدام الذاكرة لتخزين
 
 eventsRouter.post("/addEvent",auth(),authorizeRoles("Admin", "SuperAdmin"),upload.array ("images"),createEvent);
 eventsRouter.get("/getAllEvents",getAllEvents);
+eventsRouter.get("/getAllEventsReserveds",,auth(),authorizeRoles("Admin", "SuperAdmin"),getAllEventsReserveds);
 eventsRouter.get("/getEventById/:id",getEventById);
 eventsRouter.get("/getEventReservedsById/:id",auth(),authorizeRoles("Admin", "SuperAdmin"),getEventReservedsById);
 eventsRouter.delete("/deleteEvent/:id",auth(),authorizeRoles("Admin", "SuperAdmin"),deleteEvent);
  
 export default eventsRouter;
 
-
+getAllEventsReserveds 
 
 
 
