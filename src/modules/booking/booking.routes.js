2709bb98dc59e28538da95fd6beb53ecb22a719c
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-12 13:58:33 +0300

    Update booking.routes.js

--- src/modules/booking/booking.routes.js
+++ src/modules/booking/booking.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import {createBookingByWallet,deleteBooking } from "./booking.controller.js"
+import {createBookingByWallet,deleteBooking,createBookingByProof} from "./booking.controller.js"
 import multer from 'multer';   
 import { auth, authorizeRoles } from "../../middleware/auth.js";
-import { storage } from "../../utils/cloudinary.config.js"; // تأكد من مسار الملف الصحيح
-
+import { storage } from '../../utils/cloudinary.config.js';
+const upload = multer({ storage});
 const bookingRouter = express.Router();
 
 bookingRouter.post("/bookingByWallet",auth(),createBookingByWallet)
-bookingRouter.delete("/deleteBooking/:id",auth(),authorizeRoles("Admin","SuperAdmin"),deleteBooking)
-deleteBooking 
+bookingRouter.post("/bookingByProof",auth() ,upload.single('screenshot'),createBookingByProof)
+bookingRouter.delete("/deleteBooking/:id",auth(),authorizeRoles("Admin","SuperAdmin"),deleteBooking) 
 export default  bookingRouter
