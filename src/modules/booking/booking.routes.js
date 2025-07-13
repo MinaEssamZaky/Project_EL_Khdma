7b5e33c992ea984e557de6c8c7272384bbeaaa7b
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-13 08:30:10 +0300

    Update booking.routes.js

--- src/modules/booking/booking.routes.js
+++ src/modules/booking/booking.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {createBookingByWallet,deleteBooking,createBookingByProof,updateBookingStatus,getPendingBookingsForAdmin} from "./booking.controller.js"
+import {createBookingByWallet,deleteBooking,createBookingByProof,updateBookingStatus,getPendingBookingsForAdmin,getAllBookingsForAdmin } from "./booking.controller.js"
 import multer from 'multer';   
 import { auth, authorizeRoles } from "../../middleware/auth.js";
 import { storage } from '../../utils/cloudinary.config.js';
@@ -11,5 +11,6 @@ bookingRouter.post("/bookingByProof",auth() ,upload.single('screenshot'),createB
 bookingRouter.delete("/deleteBooking/:id",auth(),authorizeRoles("Admin","SuperAdmin"),deleteBooking) 
 bookingRouter.patch("/updateStatus/:id",auth(),authorizeRoles("Admin", "SuperAdmin"),updateBookingStatus);
 bookingRouter.get("/pendingBookings",auth(),authorizeRoles("Admin", "SuperAdmin"),getPendingBookingsForAdmin);
+bookingRouter.get("/getAllBookings",auth(),authorizeRoles("Admin", "SuperAdmin"),getAllBookingsForAdmin);
 
 export default  bookingRouter
