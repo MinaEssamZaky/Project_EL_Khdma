bc5fedd61d4478955875a106f482682eba0eac20
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-13 15:33:22 +0300

    Update booking.controller.js

--- src/modules/booking/booking.controller.js
+++ src/modules/booking/booking.controller.js
@@ -282,7 +282,7 @@ export const getAllBookingsForAdmin = handleError(async (req, res, next) => {
 });
 
 export const getAllBookingsForUser = handleError(async (req, res, next) => {
-  const allBookings = await bookingModel.find({user: req.user._id}).populate("user", "userName phone") 
+  const allBookings = await bookingModel.find({user: req.user._id})
   res.status(200).json({
     message: "All bookings",
     count: allBookings.length,
