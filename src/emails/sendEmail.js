0cc8a1f82bd85402fe7eb8a9328856a7a19d9e30
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-15 12:21:43 +0300

    Update sendEmail.js

--- src/emails/sendEmail.js
+++ src/emails/sendEmail.js
@@ -1,31 +1,33 @@
-// npm install nodemailer
-import nodemailer from "nodemailer";
-import { emailTemplate } from "./emailTemplate.js";
-import dotenv from "dotenv";
-dotenv.config();
-
-export const sendMail = async (email, token) => {
+export const sendMail = async (email, token, resetLink = null) => {
     try {
-        // Create transporter
         const transporter = nodemailer.createTransport({
             service: "gmail",
             auth: {
-                user:"ugmmeeting@gmail.com",
-                pass:"zeswnfgzblbgvyvx"
-            }, 
+                user: "ugmmeeting@gmail.com",
+                pass: "zeswnfgzblbgvyvx", 
+            },
         });
 
-        // Send mail
+        const subject = resetLink
+            ? "Reset Your Password"
+            : "Welcome To The UGM Meeting";
+
+        const html = resetLink
+            ? `<h3>Click the link below to reset your password:</h3>
+               <a href="${resetLink}" target="_blank">${resetLink}</a>
+               <p>This link will expire in 10 minutes.</p>`
+            : emailTemplate(token); 
+
         const info = await transporter.sendMail({
-            from: '"UGM Meeting" <ugmmeeting@gmail.com>', // sender address
-            to: email, // list of receivers
-            subject: "Welcome To The UGM Meeting", // Subject line 
-            html: emailTemplate(token) // html body
+            from: '"UGM Meeting" <ugmmeeting@gmail.com>',
+            to: email,
+            subject,
+            html,
         });
 
         return info;
     } catch (error) {
         console.error("Error sending email:", error);
-        throw error; // Re-throw the error for handling in the calling function
+        throw error;
     }
 };
