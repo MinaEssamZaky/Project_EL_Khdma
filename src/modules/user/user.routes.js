c9e7a441108a3445fb02c36c8445c389a8797e49
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-07-15 11:28:17 +0300

    Update user.routes.js

--- src/modules/user/user.routes.js
+++ src/modules/user/user.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {changePassword,deleteUserById,GitAllAdmins,GitAllUsers,LogIn,resendVerifyEmail,signUp,Updated,UpdatedRole, VerifyEmail, UpdateWallet,GetMyWalletHistory,GetMyWalletBalance,getAllBookingsForUser,ClearWalletHistory} from './user.controller.js';
+import {changePassword,deleteUserById,GitAllAdmins,GitAllUsers,LogIn,resendVerifyEmail,signUp,Updated,UpdatedRole, VerifyEmail, UpdateWallet,GetMyWalletHistory,GetMyWalletBalance,getAllBookingsForUser,ClearWalletHistory,GetRole} from './user.controller.js';
 import { auth, authorizeRoles } from '../../middleware/auth.js';
 import { ChangePasswordSchemaVal, LogInSchemaVal, signUpSchemaVal, UpdatedRoleSchemaVal, UpdateSchemaVal } from './user.validation.js';
 import { validation } from '../../middleware/Validation.js';
@@ -19,8 +19,9 @@ userRouter.put("/updateWallet/:id",auth(),UpdateWallet)
 userRouter.get("/getMyWalletHistory",auth(),GetMyWalletHistory)
 userRouter.get("/getMyWalletBalance",auth(),GetMyWalletBalance)
 userRouter.get("/getAllBookingsForUser",auth(),getAllBookingsForUser);
+userRouter.get("/getRole",auth(),GetRole);
 userRouter.patch("/clearWalletHistory/:id",auth(), ClearWalletHistory);
 
-
+ 
 
 export default userRouter;
