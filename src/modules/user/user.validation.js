9f24b78d4ed9cd8033237c64f0fa894e0c0ea756
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-09-15 08:50:47 +0300

    Update user.validation.js

--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -1,40 +1,38 @@
 import Joi from "joi"
 
 const signUpSchemaVal = Joi.object({
-    userName: Joi.string().min(2).max(20).required(),
+    firstName: Joi.string().min(2).max(10).required(),
+    secName: Joi.string().min(2).max(10).required(),
+    familyName: Joi.string().min(2).max(10).required(),
     email: Joi.string().email({ tlds: { allow: ["com"] } }).required(),
-    password: Joi.string().required(),
-    rePassword: Joi.valid(Joi.ref('password')).required(),
+    password: Joi.string().min(6).required(),
+    rePassword: Joi.valid(Joi.ref("password")).required(),
     phone: Joi.string().length(11).required(),
-})
+});
 
 const LogInSchemaVal = Joi.object({
     email: Joi.string().email({ tlds: { allow: ["com"] } }).required(),
     password: Joi.string().required(),
-})
+});
 
 const ChangePasswordSchemaVal = Joi.object({
     oldPassword: Joi.string().required(),
-    newPassword: Joi.string().required(),
-})
-
-
+    newPassword: Joi.string().min(6).required(),
+});
 
 const UpdateSchemaVal = Joi.object({
-    userName: Joi.string().min(2).max(20),
     email: Joi.string().email({ tlds: { allow: ["com"] } }),
     phone: Joi.string().length(11),
-})
+});
 
 const UpdatedRoleSchemaVal = Joi.object({
-    role: Joi.string().valid( "Admin", "User").required()
-})
-
+    role: Joi.string().valid("Admin", "User").required(),
+});
 
 export {
     signUpSchemaVal,
     LogInSchemaVal,
     UpdateSchemaVal,
     ChangePasswordSchemaVal,
-    UpdatedRoleSchemaVal
-}
+    UpdatedRoleSchemaVal,
+};
