38b5c62052cfaa7adff4edb18180074825f1f459
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-09-15 08:45:55 +0300

    Update user.model.js

--- DataBase/models/user.model.js
+++ DataBase/models/user.model.js
@@ -7,17 +7,20 @@ const userSchema = new mongoose.Schema({
         minlength: 2,
         maxlength: 10,
     },
-     secName: {
+    secName: {
         type: String,
         required: true,
         minlength: 2,
         maxlength: 10,
     },
-     familyName: {
+    familyName: {
         type: String,
         required: true,
         minlength: 2,
         maxlength: 10,
+    },
+    userName: {
+        type: String,
     },
     email: {
         type: String,
@@ -36,58 +39,67 @@ const userSchema = new mongoose.Schema({
         minlength: 11,
         maxlength: 11,
     },
-isConfirmed: {
+    isConfirmed: {
         type: Boolean,
         default: false,
     },
-    role:{
+    role: {
         type: String,
-        enum: ["SuperAdmin","Admin", "User"],
+        enum: ["SuperAdmin", "Admin", "User"],
         default: "User"
     },
-    wallet:{
-         type: Number,
-         default: 0
+    wallet: {
+        type: Number,
+        default: 0
     },
-
-   walletHistory: [{
-    amount: Number,
-    operation: String,
-    description: String,
-    performedBy: {
-        adminId: {
+    walletHistory: [
+      {
+        amount: Number,
+        operation: String,
+        description: String,
+        performedBy: {
+          adminId: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'user',
+            ref: "user",
             required: true
-        },
-        adminName: String,
-        adminRole: {
+          },
+          adminName: String,
+          adminRole: {
             type: String,
-            enum: ["SuperAdmin", "Admin","User"],
+            enum: ["SuperAdmin", "Admin", "User"],
             required: true
-        }
-    },
-    walletOwner: {
-        userId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'user',
-            required: true
+          }
         },
-        userName: String
-    },
-    previousBalance: Number,
-    newBalance: Number,
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }
-}],
-     bookings: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "booking",
-}]
-},
-{timestamps: true});
+        walletOwner: {
+          userId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user",
+            required: true
+          },
+          userName: String
+        },
+        previousBalance: Number,
+        newBalance: Number,
+        createdAt: {
+          type: Date,
+          default: Date.now
+        }
+      }
+    ],
+    bookings: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "booking"
+      }
+    ]
+}, { timestamps: true });
+
+
+userSchema.pre("save", function (next) {
+  this.userName = `${this.firstName} ${this.secName} ${this.familyName}`;
+  next();
+});
+
 
 const userModel = mongoose.model("user", userSchema);
 export default userModel;
