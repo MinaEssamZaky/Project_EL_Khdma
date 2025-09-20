ce90981f9550072990d3eb0c202818a3289a5e01
Mina Essam <140957575+MinaEssamZaky@users.noreply.github.com>
2025-09-20 09:00:49 +0300

    Update attendanceMeeting.routes.js

--- src/modules/attendance Meeting/attendanceMeeting.routes.js
+++ src/modules/attendance Meeting/attendanceMeeting.routes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { auth, authorizeRoles } from '../../middleware/auth.js';
-import { createMeeting, markAttendance,getMeetingById, getAllMeetings } from './attendanceMeeting.controller.js';
+import { createMeeting, markAttendance,getMeetingById, getAllMeetings,deleteMeeting } from './attendanceMeeting.controller.js';
 const attendanceRouter = express.Router();
 
 attendanceRouter.post("/createMeeting",auth(),authorizeRoles("SuperAdmin","Admin",),createMeeting)
 attendanceRouter.patch ("/markAttendance/:id",auth(),authorizeRoles("SuperAdmin","Admin",),markAttendance)
 attendanceRouter.get ("/getMeetingById/:id",auth(),authorizeRoles("SuperAdmin","Admin","User"),getMeetingById)
 attendanceRouter.get ("/getAllMeetings",auth(),authorizeRoles("SuperAdmin","Admin","User"),getAllMeetings)
-
+attendanceRouter.delete ("/deleteMeeting/:id",auth(),authorizeRoles("SuperAdmin","Admin","User"),deleteMeeting)
 
 
 
